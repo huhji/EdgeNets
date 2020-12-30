fe3640ae3041b15f266d511a69e9be79dbb95b05
huhji <huhji.elha@gmail.com>
2020-12-30 05:58:20 +0000

    add extract bbox module on custom.py

--- data_loader/detection/custom.py
+++ data_loader/detection/custom.py
@@ -9,12 +9,12 @@ import numpy as np
 import xml.etree.ElementTree as ET
 from PIL import Image
 
-CUSTOM_CLASS_LIST = ['__background__',
-                   'aeroplane', 'bicycle', 'bird', 'boat',
-                   'bottle', 'bus', 'car', 'cat', 'chair',
-                   'cow', 'diningtable', 'dog', 'horse',
-                   'motorbike', 'person', 'pottedplant',
-                   'sheep', 'sofa', 'train', 'tvmonitor']
+CUSTOM_CLASS_LIST = ['__background__', 'car_front', 'car_etc', 'car_back', 'car_plate', 
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '가', '거', '경', '고',
+            '구', '기', '나', '너', '노', '누', '다', '더', '도', '두', '라', '러', '로', '루',
+            '리', '마', '머', '모', '무', '바', '배', '버', '보', '부', '사', '서', '소', '수',
+            '시', '아', '어', '오', '우', '울', '임', '자', '장', '저', '조', '주', '청', '하',
+            '허', '호']
 
 class CustomDataset(data.Dataset):
 
@@ -37,11 +37,11 @@ class CustomDataset(data.Dataset):
 
         self.ids = CustomDataset._read_image_ids(search_paths)
 
-        self.data_dir = root_dir
-        self.split = split
+        # self.data_dir = root_dir
+        # self.split = split
         self.transform = transform
         self.target_transform = target_transform
-        self.keep_difficult = keep_difficult
+        # self.keep_difficult = keep_difficult
         self.CLASSES = CUSTOM_CLASS_LIST
 
         self.class_dict = {class_name: i for i, class_name in enumerate(self.CLASSES)}
@@ -78,26 +78,53 @@ class CustomDataset(data.Dataset):
                 image_path = os.path.splitext(line.rstrip().split('/')[-1])[0]
                 ids.append(image_path)
         return ids
+    
+    # extract bbox from xml
+    def _extract_bbox(search_object):
+        for bbox in search_object :
+            x1 = float(bbox.find('xmin').text)
+            y1 = float(bbox.find('ymin').text)
+            x2 = float(bbox.find('xmax').text)
+            y2 = float(bbox.find('ymax').text)
+        return x1, y1, x2, y2
 
     def _get_annotation(self, image_id):
-        annotation_file = os.path.join(self.data_dir, self.split, "Annotations", "%s.xml" % image_id)
-        objects = ET.parse(annotation_file).findall("object")
+        annotation_file = f"/data/yper_data/converted_xml/{image_id}.xml"
+        tree = ET.parse(annotation_file)
+        root = tree.getroot()
         boxes = []
         labels = []
-        is_difficult = []
-        for obj in objects:
-            class_name = obj.find('name').text.lower().strip()
-            bbox = obj.find('bndbox')
-            # VOC dataset format follows Matlab, in which indexes start from 0
-            x1 = float(bbox.find('xmin').text) - 1
-            y1 = float(bbox.find('ymin').text) - 1
-            x2 = float(bbox.find('xmax').text) - 1
-            y2 = float(bbox.find('ymax').text) - 1
-            boxes.append([x1, y1, x2, y2])
-            labels.append(self.class_dict[class_name])
-            is_difficult_str = obj.find('difficult').text
-            is_difficult.append(int(is_difficult_str) if is_difficult_str else 0)
+        main_body = []
 
+        for object in root.findall('smr_object') :
+            class_name = object.find('name').text
+            if class_name in ['car_front', 'car_etc', 'car_back'] :
+                x1, y1, x2, y2 = _extract_bbox(object.findall('bndbox'))
+
+                main_body = [x1, y1, x2, y2]
+                boxes.append(main_body)
+
+                labels.append(self.class_dict[class_name])
+                break
+            else :
+                continue
+        for object in root.findall('smr_object') :
+            class_name = object.find('name').text
+            if class_name == 'car_plate' :
+                lp_label = object.find('pose').text
+                x1, y1, x2, y2 = _extract_bbox(object.findall('bndbox'))
+                try :
+                    if (x1 < main_body[0]) or (x2 > main_body[2]) or (y1 < main_body[1]) or (y2 > main_body[3]) :
+                        continue
+                    elif lp_label in list(self.class_names) :
+                        boxes.append([x1, y1, x2, y2])
+                        labels.append(self.class_dict[lp_label])
+                except :
+                    if lp_label in list(self.class_names) :
+                        boxes.append([x1, y1, x2, y2])
+                        labels.append(self.class_dict[lp_label])
+                    else :
+                        continue
         return (np.array(boxes, dtype=np.float32),
                 np.array(labels, dtype=np.int64))
 
